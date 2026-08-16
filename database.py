6c384a932fdf37c10de12ecca43e0ddccdd919d5
janmiscik <janmiscik@gmail.com>
2026-08-16 19:55:09 +0200

    Pridana konfiguracia cez env

--- database.py
+++ database.py
@@ -1,18 +1,35 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-DATABASE_URL = "sqlite:///./digitalny-zosit.db"
+# Načítanie premenných zo súboru .env
+load_dotenv()
 
+
+# URL databázy z .env
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./digitalny-zosit.db"
+)
+
+
+# SQLAlchemy engine
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
+
+# Databázová session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+
+# Základ pre databázové modely
 Base = declarative_base()
